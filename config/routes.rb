64a7f741ee1e2faaad7849a8e0fc1def3050aed6
Marcian Diamond <marciandmnd@yahoo.com>
2015-10-28 16:56:05 +0200

    added post creation and fixed some frontend bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'user_profile/show'
-
-  get 'user_profile/edit'
-
+  resources :user do
+    resources :posts
+  end
   root "home#home"
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # The priority is based upon order of creation: first created -> highest priority.

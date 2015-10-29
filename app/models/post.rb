be45c7c4e7da4c6e815e412767a4b2857749ef48
mark jad <mark.jad@live.com>
2015-10-29 10:08:05 +0200

    changed the edit button

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :title, :message => "title can't be empty"
-  validates_presence_of :description, :message => "description can't be empty"
-  validates_presence_of :address, :message => "address can't be empty"
+  validates_presence_of :title, :message => "can't be empty"
+  validates_presence_of :description, :message => "can't be empty"
+  validates_presence_of :address, :message => "can't be empty"
 
 end
